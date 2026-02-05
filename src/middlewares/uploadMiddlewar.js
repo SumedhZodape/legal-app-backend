c897d0f4be1fd9f8024d7180aa4a8d4437960d22
Sumedh Zodape <sumedhzodape143@gmail.com>
2026-02-05 20:18:18 +0530

    file upload

---
+++ src/middlewares/uploadMiddlewar.js
@@ -0,0 +1,34 @@
+import multer from "multer";
+import path from "path"
+
+
+
+const storage = multer.diskStorage(
+    {
+        destination: function (req, file, cb){
+            // console.log(dirname)
+            cb(null, "uploads")
+        },
+        filename: function(req, file, cb){
+            const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9);
+            console.log(file)
+            // const ext = path.extname(file.originalname);
+
+            // const originalName = 
+            // console.log(ext)
+            cb(null, uniqueName+"-"+file.originalname)
+        }
+    }
+)
+
+
+
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 1* 1024 *1024  // 1MB
+    }
+})
+
+
+export default upload;
--- src/routes/clientRouter.js
+++ src/routes/clientRouter.js
@@ -1,12 +1,28 @@
 import express from 'express';
 import Protect from '../middlewares/authMiddleware.js';
 import {AITest , CreateCase} from '../controller/clientController.js'
+import upload from '../middlewares/uploadMiddlewar.js';
 
 const router = express.Router()
 
 
 router.get("/aitest", Protect, AITest);
 
-router.post("/createcase", Protect, CreateCase);
+router.post("/createcase", Protect, upload.array("proofFiles", 2),  CreateCase);
 
 export default router;
+
+
+
+
+
+// type : multipart/form-data
+
+// {
+//     "caseInfo":"Something Happen with me",
+//     "Date":"2024-12-30",
+//     "proofFiles": [
+//         file1,
+//         file2
+//     ]
+// }
