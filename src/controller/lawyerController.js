3bb73ebba7884a9776faf4b0d138a5d2f161b067
Sumedh Zodape <sumedhzodape143@gmail.com>
2026-03-20 20:18:17 +0530

    Create profile logic

--- src/controller/lawyerController.js
+++ src/controller/lawyerController.js
@@ -1,42 +1,54 @@
 import LawyerProfileModel from "../models/LawyerProfile.js";
+import UserModel from '../models/Users.js'
 
 export const CreateProfile = async (req, res) => {
 
 
-    const { userId, barCouncilId, degree, lawyerType,
+    const { email, barCouncilId, degree, lawyerType,
         experienceYears, totalCases, wonCases, lostCases, winRatio,
         feeMin, feeMax,
     } = req.body;
 
 
-    if (!userId || !barCouncilId || !degree || !lawyerType
+    if (!email || !barCouncilId || !degree || !lawyerType
         || !experienceYears
     ) {
-        return res.status(400).json({ message: "Required fileds are missing!" })
+        return res.status(400).json({ success: false, message: "Required fileds are missing!" })
     }
 
     try {
 
-        const existingUser = await LawyerProfileModel.findOne({ userId });
+        const userData = await UserModel.findOne({ email });
+
+        if (!userData) {
+            return res.status(404).json({ success: false, message: "User Doesn't exists!" })
+        }
+
+        if(userData.role !== "LAWYER"){
+            return res.status(400).json({success: false, message: `Please check your role, Role should be LAWYER and your current role is ${userData.role}`})
+        }
+
+        const existingUser = await LawyerProfileModel.findOne({ userId: userData._id });
 
         if (existingUser) {
-            return res.status(400).json({ message: "User Profile alredy exist!" })
+            return res.status(400).json({ success: false, message: "User Profile alredy exist!" })
         }
 
 
         const userProfile = await LawyerProfileModel.create(
             {
-                userId, barCouncilId, degree, lawyerType,
+                userId: userData._id, barCouncilId, degree, lawyerType,
                 experienceYears, totalCases, wonCases, lostCases, winRatio,
                 feeMin, feeMax,
             }
         )
 
-        res.status(201).json({result: userProfile, message: "User Profile created!"})
+        res.status(201).json({ success: true, result: userProfile, message: "User Profile created!" })
 
 
     } catch (error) {
-        return res.status(500).json({ message: "Server Error!" })
+        console.log(error)
+        return res.status(500).json({ success: false, message: "Server Error!" })
     }
 
 
