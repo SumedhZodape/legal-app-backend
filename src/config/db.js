2c678e243bfc3869c95e519f637413fa9160029c
Sumedh Zodape <sumedhzodape143@gmail.com>
2026-05-07 20:41:07 +0530

    test

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://sumedhzodape143_db_user:OLJcO0CsUR1SezjW@cluster0.3nk9vcb.mongodb.net/legal-app");
+        const conn = await mongoose.connect("http://");
         console.log("DB Connected...")
     } catch (error) {
         console.log(error)
